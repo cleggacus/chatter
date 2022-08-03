c2747d5d44705852c489a16145243b4943f8cccc
cleggacus <cleggacus@gmail.com>
2022-08-03 10:03:33 +0100

    a

--- src/components/Chat/ChatComponent.tsx
+++ src/components/Chat/ChatComponent.tsx
@@ -1,68 +1,94 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import styles from "../../styles/chat/chatComponent.module.scss";
 import {FiSend} from "react-icons/fi";
 import {IoMdAttach} from "react-icons/io";
 import { useUser } from "../../context/UserContext";
+import defaultPic from "../../assets/default.jpg"
+import Image from "next/image";
 
 const Chat: FC = () => {
   const { user } = useUser();
 
-  const messages = [
-    {
-      text: "Hey",
-      from: user?.username
-    },
-    {
-      text: "Hi",
-      from: "them"
-    },
-    {
-      text: "Whats up",
-      from: "them"
-    },
-    {
-      text: "nm you",
-      from: user?.username
-    },
-  ]
+  const [messages, setMessages] = useState([
+      {
+        text: "Hey",
+        from: user?.username,
+        image: defaultPic
+      },
+      {
+        text: "Hi",
+        from: "them",
+        image: defaultPic
+      },
+      {
+        text: "Whats up",
+        from: "them",
+        image: defaultPic
+      },
+      {
+        text: "nm you",
+        from: user?.username,
+        image: defaultPic
+      },
+    ]
+  )
+
+  const ref = useRef<HTMLInputElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const sendMessage = () => {
+    if(!ref.current) return;
+
+    setMessages([
+      ...messages,
+      {
+        text: ref.current.value,
+        from: user?.username,
+        image: defaultPic
+      }
+    ]);
+
+    ref.current.value = "";
+  }
+
+  useEffect(() => {
+    if(!mainRef.current) return;
+    mainRef.current.scrollTo(0, mainRef.current.scrollHeight);
+  }, [messages])
 
   return <div className={styles.container}>
-    <div className={styles.top}>
-      <div>
-        <button className={styles.outline}>Report user</button>
-        <button className={styles.outline}>Remove Friend</button>
-        <button>Goto Profile</button>
-      </div>
-    </div>
-
-    <div className={styles.main}>
-      <div>
-        <div className={styles.content}>
+      <div className={styles.content}>
+        <div className={styles.messages}>
+          <div ref={mainRef}>
           {
             messages.map(mes => {
               return <div className={`${mes.from == user?.username ? styles.me : ""} ${styles.message}`}>
+                <div className={styles.picture}>
+                  <Image src={mes.image} layout="fill" ></Image>
+                </div>
+
                 <div className={styles.messageContent}>
                   <p>{mes.text}</p>
                 </div>
               </div>
             })
           }
+          </div>
         </div>
 
         <div className={styles.input}>
           <div className={styles.inputInner}>
-            <input placeholder="Message"></input>
+            <input onKeyDown={e => {if(e.key == "Enter") sendMessage() }} ref={ref} placeholder="Message"></input>
 
             <button className={styles.noback}>
               <IoMdAttach></IoMdAttach>
             </button>
 
-            <button>
+            <button onClick={sendMessage}>
               <FiSend></FiSend>
             </button>
           </div>
         </div>
-      </div>
     </div>
   </div>
 }
